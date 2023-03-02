cb6029cc5e3c4b637022cfe2f75eeb70b13b998f
Anton Sipko <sipcoanton@gmail.com>
2023-03-02 16:44:34 +0200

    HW#30

--- Repositories/ood-ts-cipher/ts-project-ood-typescript-cipher/src/app.ts
+++ Repositories/ood-ts-cipher/ts-project-ood-typescript-cipher/src/app.ts
@@ -2,7 +2,11 @@
 
 import { CipherRightShift } from "./CipherRightShift";
 import { displayCipherDecipher } from "./displayCipherDecipher";
+import { CipherLeftShift } from "./CipherLeftShift";
 
 //module for testing
 const rightShiftCipher:CipherRightShift=new CipherRightShift(2);
 displayCipherDecipher(rightShiftCipher,"aaa");
+const leftShiftcipher: CipherLeftShift=new CipherLeftShift(1);
+displayCipherDecipher(leftShiftcipher,"ccc");
+
