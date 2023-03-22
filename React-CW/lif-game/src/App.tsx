e814f356547970097b3c3f1e9054713a2a4ed638
Anton Sipko <sipcoanton@gmail.com>
2023-03-22 20:22:00 +0200

    H36

---
+++ React-CW/lif-game/src/App.tsx
@@ -0,0 +1,15 @@
+
+import React, { ReactNode } from 'react';
+
+import './App.css';
+import { Life } from './components/Life';
+
+
+function App() {
+    
+ return <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <Life />
+ </div>
+}
+
+export default App;
