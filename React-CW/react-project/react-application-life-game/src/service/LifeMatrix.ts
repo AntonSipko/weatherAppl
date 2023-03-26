099bc4e06fb275f0883e1182652f357a1dfcb1ae
Anton Sipko <sipcoanton@gmail.com>
2023-03-26 19:42:18 +0300

    HW#37

--- React-CW/react-project/react-application-life-game/src/service/LifeMatrix.ts
+++ React-CW/react-project/react-application-life-game/src/service/LifeMatrix.ts
@@ -1,42 +1,39 @@
 import { getRandomMatrix } from "../util/random";
 
 export default class LifeMatrix {
-    constructor(private _numbers: number[][]) { }
+    constructor(private _numbers: number[][]){}
     get numbers() {
         return this._numbers;
     }
     nextStep(): number[][] {
-
-        //FIX in HW #36
-        const matrix = this._numbers;
-        this._numbers = matrix.map((column,index )=> {
-            const lastIndex=index;
-            let count: number = 0;
-            const lifeCellIndex = column.findIndex((number) => number == 1,index);
-            if (lifeCellIndex > 0 && lifeCellIndex < matrix.length) {
-                const checkingArea = matrix.slice(lifeCellIndex - 1, lifeCellIndex + 2);
-                checkingArea.map(row => {
-                    const rowsArea: number[] = row.slice(lifeCellIndex - 1, lifeCellIndex + 2)
-                    count += rowsArea.reduce((res, num) => res + num)
-                    if (count > 3 ||count<2) {
-                        row[lifeCellIndex] = 0;
-                    }
-                    return row;
-                })
-            } else {
-                const checkingArea = matrix.slice(lifeCellIndex, lifeCellIndex + 2);
-                checkingArea.map(row => {
-                    const rowsArea: number[] = row.slice(lifeCellIndex, lifeCellIndex + 2)
-                    count += rowsArea.reduce((res, num) => res + num)
-                    if (count > 3 || count<2) {
-                        row[lifeCellIndex] = 0;
-                    }
-                    return row;
-                })
-            }
-            return column;
-        })
-
+        
+        this._numbers = this._numbers.map((__, i) => this.getNewRow(i));
         return this._numbers;
     }
+    getNewRow(i: number): number[] {
+        return this._numbers[i].map((__, j) => this.getNewCell(i, j));
+    }
+    
+    getNewCell(i: number, j: number): number {
+        const matrixPart: number[][] = this.getMatrixPart(i, j);
+        const sumCells = matrixPart.reduce((res, row) => res + 
+        row.reduce((rSum, cell) => rSum + cell), 0);
+        return this._numbers[i][j] ? isLiveFromLive(sumCells) : isLiveFromDead(sumCells);
+    }
+    getMatrixPart(i: number, j: number): number[][] {
+        const sliceIndex1 = j == 0 ? j : j - 1;
+        const sliceIndex2 = j == this._numbers[0].length - 1 ? j + 1 : j + 2;
+        const savedNum = this._numbers[i][j];
+        this._numbers[i][j] = 0;
+        const res = [i - 1, i, i + 1].map(rowIndex => this._numbers[rowIndex] ? 
+            this._numbers[rowIndex].slice(sliceIndex1, sliceIndex2) : [0]);
+       this._numbers[i][j] = savedNum;
+        return res;
+    }
+}
+function isLiveFromLive(sumCells: number): number {
+    return +(sumCells == 2 || sumCells == 3);
+}
+function isLiveFromDead(sumCells: number): number {
+    return +(sumCells == 3);
 }
